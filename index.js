26e7853fffedf6679b59ca5e6f8fc2b613cc8b59
AnastaciusWright <alexandronadal@gmail.com>
2020-03-14 17:49:51 -0300

    Bot funcional

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const config = require("./config.json");
 
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message',message =>{
-    console.log(message.content);
-    });
-client.login('Njg4NDYxOTY3MDEwNDMxMjUz.Xm0q7A.c5EVw7C9KGJwQpkzF6cxuxQ6-Yk');
+    if(message.content ==='!ping')
+    {
+        message.channel.send('Pong.');
+    }
+  if(message.content =='!consulta')
+  {
+    message.channel.send("El estado actual del usuario es "+message.member.voice.serverMute);
+    message.channel.send("Intentando desmutear");
+    //aca hay que desmutearlos
+    message.member.voice.setMute(false,"consulta");
+    setTimeout(function(){message.member.voice.setMute(true,"consulta fin")},10000)
+  }
+});
+
+client.login(config.token);
